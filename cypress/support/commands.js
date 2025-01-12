64d3508e7966c13431041bf74c0f9aa6a43ed4bf
nityanandbb <nityanand.bharati@qed42.com>
2025-01-13 02:00:32 +0530

    Implement Automated Utility Function Loader for Cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,12 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Importing all the util functions.
+
+// cypress/support/commands.js
+import * as utils from "./utilsLoader";
+
+Object.keys(utils).forEach((key) => {
+  Cypress.Commands.add(key, utils[key]);
+});
